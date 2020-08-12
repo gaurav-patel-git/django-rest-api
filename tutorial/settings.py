d6c615dc56c98174699bfe54ac5b106866a0404f
Gaurav Patel <gp8966891720@gmail.com>
2020-08-12 15:04:12 +0530

    update settings file

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vx=#tnxdn0-a0*xx81sn7k$^*lo8yry6oe2hktsb1c7*b=a2sb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -121,5 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
